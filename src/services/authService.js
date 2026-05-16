f80ca5c000286850de0d36f4b797a072be659413
keerthika0909 <167891756+keerthika0909@users.noreply.github.com>
2026-05-16 17:16:34 +0530

    Delete src directory

--- src/services/authService.js
+++
@@ -1,7 +0,0 @@
-import { request } from "./api";
-
-export const registerUser = (data) =>
-  request("/auth/register", "POST", data);
-
-export const loginUser = (data) =>
-  request("/auth/login", "POST", data);
--- src/services/newsService.js
+++
@@ -1,6 +0,0 @@
-import { request } from "./api";
-
-export const getTopNews = () => request("/news/top");
-
-export const searchNews = (q) =>
-  request(`/news/search?q=${q}`);
