e79ae2ffc8c6b5a717dda5ea346a63acc3887d7e
Keerthika Badri <badrikeerthika@gmail.com>
2026-05-16 16:58:06 +0530

    Fixed Railway CORS issue

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,23 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors({
-  origin: "https://news-app-llrm.vercel.app"
-}));
+// CORS
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Backend Running Successfully");
+});
+
+// Routes
 app.use("/auth", require("./routes/authRoutes"));
 app.use("/news", require("./routes/newsRoutes"));
 app.use("/fav", require("./routes/favouriteRoutes"));
